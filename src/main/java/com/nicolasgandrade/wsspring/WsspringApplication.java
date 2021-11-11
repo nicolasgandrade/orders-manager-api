d7fac84997b7db26e6a24f6fed83c32591b2c6d5
Nicolas Guerrero <nicolasguerrero72@gmail.com>
2021-11-11 16:27:24 -0300

    project created

---
+++ src/main/java/com/nicolasgandrade/wsspring/WsspringApplication.java
@@ -0,0 +1,13 @@
+package com.nicolasgandrade.wsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicolasgandrade/wsspring/WsspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolasgandrade.wsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
