a5b60bc27db1e19f9a6328966d5e321f0c1d855a
Nicolas Guerrero <nicolasguerrero72@gmail.com>
2021-11-27 15:34:32 -0300

    feat: add OrderStatus enum and implements it

--- src/main/java/com/nicolasgandrade/wsspring/config/TestConfig.java
+++ src/main/java/com/nicolasgandrade/wsspring/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.nicolasgandrade.wsspring.entities.Order;
 import com.nicolasgandrade.wsspring.entities.User;
+import com.nicolasgandrade.wsspring.entities.enums.OrderStatus;
 import com.nicolasgandrade.wsspring.repositories.OrderRepository;
 import com.nicolasgandrade.wsspring.repositories.UserRepository;
 
@@ -32,9 +33,9 @@ public class TestConfig implements CommandLineRunner{
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "888888", "123456");
 		
 		//				    (ID a ser gerado, data no formato ISO 8601, usuário que pediu)
-		Order o1 = new Order(null, Instant.parse("2021-11-27T14:36:02Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2021-11-28T14:36:02Z"), u1);
-		Order o3 = new Order(null, Instant.parse("2021-11-29T14:36:02Z"), u2);
+		Order o1 = new Order(null, Instant.parse("2021-11-27T14:36:02Z"), OrderStatus.DELIVERED, u1);
+		Order o2 = new Order(null, Instant.parse("2021-11-28T14:36:02Z"), OrderStatus.PAID, u1);
+		Order o3 = new Order(null, Instant.parse("2021-11-29T14:36:02Z"), OrderStatus.PAID, u2);
 		
 		//Recebe uma lista dos objetos a serem salvos; A lista está sendo criada inline;
 		userRepository.saveAll(Arrays.asList(u1, u2));
