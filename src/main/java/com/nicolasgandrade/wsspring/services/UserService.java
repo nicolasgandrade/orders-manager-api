de5f22cca2308e7424d1d513ae60547edb43e388
Nicolas Guerrero <nicolasguerrero72@gmail.com>
2021-11-30 17:52:37 -0300

    feat: User update

--- src/main/java/com/nicolasgandrade/wsspring/services/UserService.java
+++ src/main/java/com/nicolasgandrade/wsspring/services/UserService.java
@@ -33,4 +33,17 @@ public class UserService {
 		repository.deleteById(id);
 	}
 	
+	public User update(Long id, User obj) {
+		@SuppressWarnings("deprecation")
+		User entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());		
+	}
+	
 }
