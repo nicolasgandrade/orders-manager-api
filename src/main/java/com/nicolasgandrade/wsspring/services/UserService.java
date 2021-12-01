d67659c756f83f4bce7927db88c4b922b9361c65
Nicolas Guerrero <nicolasguerrero72@gmail.com>
2021-11-30 22:03:55 -0300

    feat: add Update exception handler

--- src/main/java/com/nicolasgandrade/wsspring/services/UserService.java
+++ src/main/java/com/nicolasgandrade/wsspring/services/UserService.java
@@ -3,6 +3,8 @@ package com.nicolasgandrade.wsspring.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -44,10 +46,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		@SuppressWarnings("deprecation")
-		User entity = repository.getOne(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {			
+			@SuppressWarnings("deprecation")
+			User entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
--- src/main/java/com/nicolasgandrade/wsspring/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/nicolasgandrade/wsspring/services/exceptions/ResourceNotFoundException.java
@@ -4,6 +4,6 @@ public class ResourceNotFoundException extends RuntimeException{
 	private static final long serialVersionUID = 1L;
 
 	public ResourceNotFoundException(Object id) {
-		super("Resource not found. Id " + id);
+		super("Resource not found:  id " + id);
 	}
 }
